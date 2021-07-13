6ba31354c882b40d5784f58cda1e2e3b7865b908
vancedy105 <vancedy105@gmail.com>
2021-07-13 12:24:22 -0400

    password leaked

--- backend/db/mongo.js
+++ backend/db/mongo.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose')
 
-
-// mongoose.connect(`mongodb+srv://elon:${process.env.MONGODB_ADMIN_PASSWORD}@testcluster.qahcc.mongodb.net/myFirstDatabase?retryWrites=true/items`, {useNewUrlParser: true, useUnifiedTopology: true})
-
-mongoose.connect('mongodb+srv://elon:digger@testcluster.qahcc.mongodb.net/items?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(`mongodb+srv://elon:${process.env.MONGO_ELON}@testcluster.qahcc.mongodb.net/items?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
 
 
 const db = mongoose.connection;
